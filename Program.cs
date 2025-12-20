6a21f4af50892517e607a1128e17035be4802da1
Kamil Starosta <ks@g3w.eu>
2025-12-20 17:06:22 +0100

    Added Menu and choice reader
    	Added menu of actions and a choice reader with a parsing of
    number

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello World!");
+﻿Console.WriteLine("====Simple Calculator====");
+Console.WriteLine("1. Addition");
+Console.WriteLine("2. Substraction");
+Console.WriteLine("3. Multiplication");
+Console.WriteLine("4. Division");
+Console.WriteLine("5. Exit");
+Console.WriteLine("=========================");
+
+Console.Write(">> ");
+string? userInput = Console.ReadLine();
+
+if (int.TryParse(userInput, out int choice))
+{
+    // Valid number
+}
+else
+{
+    Console.WriteLine("Invalid input, not a number.");
+}
+
