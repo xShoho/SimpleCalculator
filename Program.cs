9e7627c392ffb8d96d16eb13372b82e66b07134b
Kamil Starosta <ks@g3w.eu>
2025-12-20 17:32:41 +0100

    Added Addition logic

--- Program.cs
+++ Program.cs
@@ -15,6 +15,43 @@ if (int.TryParse(userInput, out int choice))
 }
 else
 {
-    Console.WriteLine("Invalid input, not a number.");
+    Console.WriteLine("Invalid input, not a number");
+}
+
+switch (choice)
+{
+    case 1:
+        Console.WriteLine("Provide an x and y numbers");
+        Console.Write("x: ");
+        string? x = Console.ReadLine();
+        Console.Write("y: ");
+        string? y = Console.ReadLine();
+
+        if (double.TryParse(x, out double num1) && double.TryParse(y, out double num2))
+        {
+            Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+        }
+
+        break;
+    case 2:
+        // Substraction logic
+        Console.WriteLine("Not yet implemented");
+        break;
+    case 3:
+        // Multiplication logic
+        Console.WriteLine("Not yet implemented");
+        break;
+    case 4:
+        // Division logic
+        Console.WriteLine("Not yet implemented");
+        break;
+    case 5:
+        Console.WriteLine("Exit");
+
+        break;
+    default:
+        Console.WriteLine("Invalid option, provide number 1-5");
+
+        break;
 }
 
