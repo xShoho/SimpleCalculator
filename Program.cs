5d9bab075784aec4b227d555a584139c79647f5e
Kamil Starosta <ks@g3w.eu>
2025-12-23 23:40:56 +0100

    Changed to not exit after invalid inputs

--- Program.cs
+++ Program.cs
@@ -15,127 +15,118 @@ while (running)
 
     if (int.TryParse(userInput, out int choice))
     {
-        // Valid number
+        Console.Clear();
+
+        switch (choice)
+        {
+            case 1:
+                Console.WriteLine("Provide an x and y numbers");
+                Console.Write("x: ");
+                string? addX = Console.ReadLine();
+                Console.Write("y: ");
+                string? addY = Console.ReadLine();
+
+                if (
+                    double.TryParse(addX, out double addXDouble)
+                    && double.TryParse(addY, out double addYDouble)
+                )
+                {
+                    Console.WriteLine($"{addXDouble} + {addYDouble} = {addXDouble + addYDouble}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, not a number");
+                    break;
+                }
+
+                break;
+            case 2:
+                Console.WriteLine("Provide an x and y numbers");
+                Console.Write("x: ");
+                string? subX = Console.ReadLine();
+                Console.Write("y: ");
+                string? subY = Console.ReadLine();
+
+                if (
+                    double.TryParse(subX, out double subXDouble)
+                    && double.TryParse(subY, out double subYDouble)
+                )
+                {
+                    Console.WriteLine($"{subXDouble} - {subYDouble} = {subXDouble - subYDouble}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, not a number");
+                    break;
+                }
+
+                break;
+            case 3:
+                // Multiplication logic
+                Console.WriteLine("Provide an x and y numbers");
+                Console.Write("x: ");
+                string? mulX = Console.ReadLine();
+                Console.Write("y: ");
+                string? mulY = Console.ReadLine();
+
+                if (
+                    double.TryParse(mulX, out double mulXDouble)
+                    && double.TryParse(mulY, out double mulYDouble)
+                )
+                {
+                    Console.WriteLine($"{mulXDouble} * {mulYDouble} = {mulXDouble * mulYDouble}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, not a number");
+                    break;
+                }
+
+                break;
+            case 4:
+                Console.WriteLine("Provide an x and y numbers");
+                Console.Write("x: ");
+                string? divX = Console.ReadLine();
+                Console.Write("y: ");
+                string? divY = Console.ReadLine();
+
+                if (
+                    double.TryParse(divX, out double divXDouble)
+                    && double.TryParse(divY, out double divYDouble)
+                )
+                {
+                    if (divYDouble != 0)
+                    {
+                        Console.WriteLine(
+                            $"{divXDouble} / {divYDouble} = {divXDouble / divYDouble}"
+                        );
+                    }
+                    else
+                    {
+                        Console.WriteLine("Cannot divide by 0");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, not a number");
+                    break;
+                }
+
+                break;
+            case 5:
+                Console.WriteLine("Exit");
+                running = false;
+
+                break;
+            default:
+                Console.WriteLine("Invalid option, provide number 1-5");
+
+                break;
+        }
     }
     else
     {
         Console.WriteLine("Invalid input, not a number");
-        running = false;
-        break;
-    }
-
-    Console.Clear();
-
-    switch (choice)
-    {
-        case 1:
-            Console.WriteLine("Provide an x and y numbers");
-            Console.Write("x: ");
-            string? addX = Console.ReadLine();
-            Console.Write("y: ");
-            string? addY = Console.ReadLine();
-
-            if (
-                double.TryParse(addX, out double addXDouble)
-                && double.TryParse(addY, out double addYDouble)
-            )
-            {
-                Console.WriteLine($"{addXDouble} + {addYDouble} = {addXDouble + addYDouble}");
-            }
-            else
-            {
-                Console.WriteLine("Invalid input, not a number");
-                running = false;
-                break;
-            }
-
-            break;
-        case 2:
-            // Substraction logic
-            Console.WriteLine("Provide an x and y numbers");
-            Console.Write("x: ");
-            string? subX = Console.ReadLine();
-            Console.Write("y: ");
-            string? subY = Console.ReadLine();
-
-            if (
-                double.TryParse(subX, out double subXDouble)
-                && double.TryParse(subY, out double subYDouble)
-            )
-            {
-                Console.WriteLine($"{subXDouble} - {subYDouble} = {subXDouble - subYDouble}");
-            }
-            else
-            {
-                Console.WriteLine("Invalid input, not a number");
-                running = false;
-                break;
-            }
-
-            break;
-        case 3:
-            // Multiplication logic
-            Console.WriteLine("Provide an x and y numbers");
-            Console.Write("x: ");
-            string? mulX = Console.ReadLine();
-            Console.Write("y: ");
-            string? mulY = Console.ReadLine();
-
-            if (
-                double.TryParse(mulX, out double mulXDouble)
-                && double.TryParse(mulY, out double mulYDouble)
-            )
-            {
-                Console.WriteLine($"{mulXDouble} * {mulYDouble} = {mulXDouble * mulYDouble}");
-            }
-            else
-            {
-                Console.WriteLine("Invalid input, not a number");
-                running = false;
-                break;
-            }
-
-            break;
-        case 4:
-            // Division logic
-            Console.WriteLine("Provide an x and y numbers");
-            Console.Write("x: ");
-            string? divX = Console.ReadLine();
-            Console.Write("y: ");
-            string? divY = Console.ReadLine();
-
-            if (
-                double.TryParse(divX, out double divXDouble)
-                && double.TryParse(divY, out double divYDouble)
-            )
-            {
-                if (divYDouble != 0)
-                {
-                    Console.WriteLine($"{divXDouble} / {divYDouble} = {divXDouble / divYDouble}");
-                }
-                else
-                {
-                    Console.WriteLine("Cannot divide by 0");
-                    running = false;
-                    break;
-                }
-            }
-            else
-            {
-                Console.WriteLine("Invalid input, not a number");
-                running = false;
-                break;
-            }
-
-            break;
-        case 5:
-            Console.WriteLine("Exit");
-            running = false;
-
-            break;
-        default:
-            Console.WriteLine("Invalid option, provide number 1-5");
-
-            break;
     }
 }
